d4ca564fc6932b07af0bd728fee0546f67cd28e8
aUnicornDev <86775297+aUnicornDev@users.noreply.github.com>
2022-01-07 22:34:03 +0530

    CU-1y2tfhd : Adding functionality with VueJS

---
+++ index.js
@@ -0,0 +1,68 @@
+
+const jobList = {
+    data() {
+      return {
+        jobs:[],
+        appliedFilters:[],
+        count:1
+      }
+    },
+    
+    mounted(){
+       fetch('./data.json')
+       .then(data=>{
+           return data.json()
+       }).
+       then( res=>{
+           this.jobs = res;
+       })
+       .catch(err=>{
+           console.log(err)
+       })
+    },
+    methods:{
+        applyFilter(filter){
+            
+            if(!this.appliedFilters.includes(filter)){
+                this.appliedFilters.push(filter)
+            }
+        }
+
+    }
+    ,
+    computed:{
+        addJobFilters(){
+            this.jobs.forEach(job=>{
+                job.filters = [];
+                job.filters.push(job.role);
+                job.filters.push(job.level);
+                job.filters=job.filters.concat(job.languages);
+                job.filters=job.filters.concat(job.tools);
+                
+
+            })
+        },
+        filteredJobs(){
+            this.addJobFilters
+            if (this.appliedFilters.length){
+
+                return this.jobs.filter(job=>{
+                    let isFiltered = true;
+                    this.appliedFilters.forEach(filter=>{
+
+                        if(!job.filters.includes(filter)){
+                            isFiltered = false
+                        }
+                    })
+                    return isFiltered;
+                })
+                
+            }
+            return this.jobs;
+
+        }
+    }
+
+  }
+  
+  Vue.createApp(jobList).mount('.filteredList')
